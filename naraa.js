a4bd74176db36481f0287993f0c135d3b5f3d0d4
NaraAnimation <bimdindonesia@gmail.com>
2025-01-13 02:34:07 +0700

    Delete naraa.js

--- naraa.js
+++
@@ -1,9 +0,0 @@
-var video = document.querySelector('video');
-video.style.width = '100%'; // Atur lebar video
-video.style.height = '300px';
-const radioButtons = document.querySelectorAll('input[type="radio"]');
-
-radioButtons.forEach(button => {
-button.style.width = '20px';
-button.style.height = '20px';
-});  
